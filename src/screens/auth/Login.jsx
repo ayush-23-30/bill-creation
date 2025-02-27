56ed235de4504a7bf8b22e650e4e1e09eb82034b
Ayushreelax <146746299+ayush-23-30@users.noreply.github.com>
2025-02-27 18:34:32 +0530

    Bill Creation

---
+++ src/screens/auth/Login.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/Logo.png";
+import hero from "../../assets/login_img.avif";
+
+function Login() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  function loginInputField() {
+    return (
+      <div className="border-2 h-52 mt-10 w-[90%] sm:w-[90%] md:w-[80%]"></div>
+    );
+  }
+
+  function loginScreenData() {
+    return (
+      <div className="w-full lg:w-1/2 flex flex-col justify-center items-start px-10 md:px-20 h-screen">
+        <img src={logo} width={150} className="relative bottom-12 md:bottom-0" />
+        <div className="mt-6 md:mt-12">
+          <h2 className="text-3xl font-bold">Welcome back!</h2>
+          <p className="text-gray-500 text-sm leading-2">Please Enter your Details.</p>
+        </div>
+
+        {/* Login Fields */}
+        {loginInputField()}
+
+        {/* Divider */}
+        <div className="w-[90%] sm:w-[90%] md:w-[80%] my-4 flex justify-center items-center">
+          <div className="border-[1px] border-[#D0D5DD] flex-grow"></div>
+          <p className="text-lg mx-2 relative -top-[2px]">or</p>
+          <div className="border-[1px] border-[#D0D5DD] flex-grow"></div>
+        </div>
+
+        {/* Sign-Up Button */}
+        <div className="border-[1px] cursor-pointer w-[90%] sm:w-[90%] md:w-[80%] border-[#D0D5DD] rounded-md shadow-md text-center hover:bg-gray-200 transition">
+          <p className="flex justify-center font-semibold items-center p-2">
+            Don't have an account?{" "}
+            <a href="#" className="text-blue-500 pl-1 underline hover:text-blue-700">
+              Sign-Up
+            </a>
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  function imageSideSection() {
+    return (
+      <div className="hidden lg:flex items-center justify-center w-1/2 h-screen">
+        <img 
+          src={hero} 
+          className="w-full h-[calc(100vh-40px)] m-5 rounded-lg object-cover" 
+          alt="Login Hero"
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full h-screen flex flex-col lg:flex-row justify-center lg:justify-start items-center overflow-hidden">
+      {loginScreenData()}
+      {imageSideSection()}
+    </div>
+  );
+}
+
+export default Login;
